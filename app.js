aec3d20f983405bde12df729fc3cdd47c0bc8e44
Rahmeen14 <rahmeenwill99@gmail.com>
2017-10-16 19:14:12 +0530

    few other errors removed

--- app.js
+++ app.js
@@ -25,11 +25,12 @@ app.get("/login", function(req, res){
 });
 app.post("/login", function(req, res){
 	//console.log(req);
+ 
 	 connection.query(
   'SELECT * FROM users WHERE user_name = ?',
   [req.body.username],
   function (err, result) {
-  	//console.log(result);
+  	console.log(result[0]);
   	var ui=result[0].user_id;
     //console.log("user id is"+ui);
   	
@@ -42,13 +43,23 @@ app.post("/login", function(req, res){
     	if(k.password===req.body.password)
     	{
         connection.query(
-          'SELECT * FROM photos WHERE photo_user_id = ?',
+          'SELECT * FROM person WHERE userid = ?',
          [ui],
           function (err, result) {
           if (err) throw err;
-            //console.log(result);
-          res.render("personprofile",{u:k,p:result});
-        //res.render("personprofile",{u:result[0]});
+          else
+          {
+              if(result[0]===undefined)
+               {
+              var routey="/page/"+ui+"/";
+                res.redirect(routey);
+                
+           } 
+              else
+              {
+                res.redirect("/person/"+ui+"/");
+              }
+          }
       });
 
      }
@@ -96,7 +107,7 @@ app.post("/register", function(req, res){
 app.post("/register/page", function(req, res){
   // console.log("req",req.body);
   var pages={
-    "userid":req.body.userid,
+    "user__id":req.body.userid,
     "purpose":req.body.purpose,
     "page_title":req.body.pagetitle
   }
@@ -106,9 +117,9 @@ app.post("/register/page", function(req, res){
     res.send({
       "code":400,
       "failed":"error ocurred"
-    })
+    });
   }else{
-    console.log('The solution is: ', results);
+  
     res.redirect("/page/"+results.insertId);
   }
 });
@@ -127,10 +138,9 @@ app.post("/register/person", function(req, res){
     res.send({
       "code":400,
       "failed":"error ocurred"
-    })
+    });
   }else{
-    console.log('The solution is: ', results);
-    res.redirect("/login");
+        res.redirect("/login");
   }
 });
 });
@@ -159,43 +169,58 @@ app.get("/person/:id",function(req,res){
   	//console.log(u);
   });	
 });
-// app.get("/page/:id",function(req,res){
-// 	//res.render("profile",{id:req.params.id});
-// 	var u={}, r;
-// 	//const r;
-// 	var today = new Date();
-// 	connection.query(
-//   'SELECT * FROM page WHERE page_id = ?',
-//   [req.params.id],
-//   function (err, result) {
-//     if (err) console.log("e7");
+app.get("/page/:id",function(req,res){
+	//res.render("profile",{id:req.params.id});
+  console.log("route hit");
+	var u={}, r;
+	//const r;
+	var today = new Date();
+	connection.query(
+  'SELECT * FROM page WHERE user__id = ?',
+  [req.params.id],
+  function (err, result) {
+    if (err)
+    console.log("e7");
 
-//     //console.log('SEARCH ' + result[0].first_name + ' rows');
-//     u["page_title"]=result[0].page_title;
-//     u["purpose"]=result[0].purpose;
-//     u["userid"]=result[0].userid;
-//     r=result[0].userid;
-//     console.log(u);
-//     connection.query(
-//   'SELECT * FROM users WHERE user_id = ?',
-//   [r],
-//   function (err, result) {
-//     if (err) console.log("e8");
+    console.log("lalala");
+    console.log(result[0]);
+    
+    u["page_title"]=result[0].page_title;
+    u["purpose"]=result[0].purpose;
+    u["user__id"]=req.params.id;
+    r=req.params.id;
+ 
+    connection.query(
+  'SELECT * FROM users WHERE user_id = ?',
+  [r],
+  function (err, result) {
+    if (err) console.log("e8");
 
-//     console.log(result[0]);
-//     u["user_name"]=result[0].user_name;
-//     u["email_id"]=result[0].email_id;
-//   	u["password"]=result[0].password;
-//   	u["profile_pic"]=result[0].profile_pic;
-//   	u["created_at"]=today;
-//   	res.render("pageprofile",{u:u});
-//   	console.log(u);
-//   }
-// );	
-//   }
-// );	//connection.close();
+ 
+    u["user_name"]=result[0].user_name;
+    u["email_id"]=result[0].email_id;
+  	u["password"]=result[0].password;
+  	u["profile_pic"]=result[0].profile_pic;
+  	u["created_at"]=today;
+
+     connection.query(
+          'SELECT * FROM photos WHERE photo_user_id = ?',
+         [r],
+          function (err, result) {
+          if (err) console.log("err");
+           // console.log(result);
+          res.render("pageprofile",{u:u,p:result});
+        //res.render("personprofile",{u:result[0]});
+        //console.log("sent user:")
+      });
+
+  	
+  }
+);	
+  }
+);	//connection.close();
 	
-// });
+});
 app.get("/person/:id/newpersonpost",function(req,res){
 	res.render("partials/newpersonpost",{id:req.params.id});
 	});
@@ -231,7 +256,7 @@ app.post("/person/:id/newpersonpost",function(req,res){
    
   }
   var hashtags=[];
-  console.log(arr);
+ 
    connection.query('INSERT INTO photos SET ?',pic, function (error, results, fields) {
   if (error) {
     console.log("error ocurred",error);
@@ -240,8 +265,8 @@ app.post("/person/:id/newpersonpost",function(req,res){
       "failed":"error ocurred"
     })
   }else{
-  	console.log("results are");
-  	console.log(results);
+  	
+  	
     for(j=0;j<arr.length;++j){
       var temp=[];
       temp.push(arr[j],results.insertId,today);
@@ -279,6 +304,93 @@ app.post("/person/:id/newpersonpost",function(req,res){
 
 	});
 });
+app.get("/page/:id/newpagepost", function(req, res){
+  res.render("partials/newpagepost", {id: req.params.id});
+});
+
+app.post("/page/:id/newpagepost",function(req,res){
+  var today = new Date();
+  var pic={
+    "image_url":req.body.imageurl,
+    "caption":req.body.caption,
+    "created_at":today,
+    "photo_user_id":req.params.id,
+    "photo_tag_text":req.body.tags
+  }
+  var t=req.body.tags;
+  var arr=[];
+  var k=0;
+  for(var i=0;i<t.length;++i)
+  {
+    if(t[i]==="#")
+    {
+      i++;
+      while(t[i]!="#")
+      {
+        ++i;
+        if(i>=t.length)
+        {
+          break;
+        }
+      }
+       arr.push(t.substring(k+1, i));
+      k=i;
+      i--;
+    }
+   
+  }
+  var hashtags=[];
+ 
+   connection.query('INSERT INTO photos SET ?',pic, function (error, results, fields) {
+  if (error) {
+    console.log("error ocurred",error);
+    res.send({
+      "code":400,
+      "failed":"error ocurred"
+    })
+  }else{
+    
+    
+    for(j=0;j<arr.length;++j){
+      var temp=[];
+      temp.push(arr[j],results.insertId,today);
+      hashtags.push(temp);
+    }
+    console.log(hashtags);
+
+     connection.query(
+  'SELECT * FROM users WHERE user_id = ?',
+  [req.params.id],
+  function (err, result) {
+      console.log("e1");
+     var m=result[0];
+     connection.query(
+
+          'SELECT * FROM photos WHERE photo_user_id = ?',
+         [req.params.id],
+          function (err, result) {
+          if (err) console.log("e5");
+
+          //console.log("u cannot reach me");
+            //console.log(result);
+            var sql = "INSERT INTO hashtags (tag_name,tag_photo_id,created_at) VALUES ?";
+            connection.query(sql, [hashtags], function(err) {
+               if (err) throw err;
+              });
+          res.render("pageprofile",{u:m,p:result});
+
+         // res.redirect("/person/"+req.params.id+"/");
+        //res.render("personprofile",{u:result[0]});
+      });
+    //console.log('The solution is: ', req.body.page);
+});
+        
+  }
+
+  });
+});
+
+
 app.get("/person/:id/follows",function(req,res){
     var id=req.params.id;
      connection.query(
@@ -474,12 +586,16 @@ app.get("/person/:id/photos/:photo_id/like/:like_creator/new",function(req,res){
     })
   }else
   {
-    //console.log("results are"+results);
+    //s("results are"+results);
    // res.send("h");
     res.redirect("/person/"+req.params.like_creator+"/feeds");
   }
   });
 });
+app.get("/page/profile/:id", function(req, res){
+  res.render("page", {});
+});
+
 app.listen(3000, 'localhost',function(){
 	console.log("server on duty, mallady");
 });
